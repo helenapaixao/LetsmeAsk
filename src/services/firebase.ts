9b70c3f410edc485aa63555a6d02eba4a5dc8a2d
Helena Paixão <hp.helenapaixao@gmail.com>
2021-07-11 13:21:42 -0300

    fix: config env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,19 +3,19 @@ import firebase from 'firebase'
 import 'firebase/auth';
 import 'firebase/database'
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAtWP6vljdOBBSBMdJ6B5grcy2ql2Ux5Vg",
-    authDomain: "letmeask-6a903.firebaseapp.com",
-    databaseURL: "https://letmeask-6a903-default-rtdb.firebaseio.com",
-    projectId: "letmeask-6a903",
-    storageBucket: "letmeask-6a903.appspot.com",
-    messagingSenderId: "520320415810",
-    appId: "1:520320415810:web:92b5fd11b38423992c15db",
-    measurementId: "G-XMQQKNNNKZ"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-  
+
 export const auth = firebase.auth();
- export const database = firebase.database();
+export const database = firebase.database();
