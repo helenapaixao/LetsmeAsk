7e80beab80f9fbfe8281371aaefcf469ea2f1b3a
Helena Paixão <hp.helenapaixao@gmail.com>
2021-07-11 14:41:14 -0300

    feat: config sass in project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
-
-
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
 export function Home() {
   return (
     <div>
       <aside>
-        
+        <img src={illustrationImg} alt="Ilustração" />
+        <strong>Crie salas  de Q&amp;A  ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+
       </aside>
-  </div>
+      <main>
+
+        <div>
+        <img src={logoImg} alt="Logo" />
+        </div>
+        <button>
+             <img src={googleIconImg} alt="Button" />
+          Crie sua sala com Google
+        </button>
+        <div>
+          ou entre em uma sala
+        </div>
+
+        <form >
+          <input type="text"
+          placeholder="Digite o código da sala"
+          />
+          <button type="submit">Entrar na sala</button>
+
+        </form>
+
+      </main>
+ </div>
 )
 }
 
