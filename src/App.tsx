3d44bd22cbac851635688e9d87ad84650f516bd0
Helena Paixão <hp.helenapaixao@gmail.com>
2021-07-18 10:43:49 -0300

    feat: create new room page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Home from './pages/Home'
+/* import Home from './pages/Home'
+ */
 
 
+import NewRoom from './pages/NewRoom'
 function App() {
   return (
-  <Home/>
+  <NewRoom/>
   );
 }
 
