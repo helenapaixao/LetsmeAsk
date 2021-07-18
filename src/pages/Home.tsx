34d0d94bc4b7cdb0bd2474e9f59ecd2b92a349ee
Helena Paixão <hp.helenapaixao@gmail.com>
2021-07-18 10:25:30 -0300

    config button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,32 +11,27 @@ export function Home() {
         <img src={illustrationImg} alt="Ilustração" />
         <strong>Crie salas  de Q&amp;A  ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
-
       </aside>
       <main>
-
-        <div>
-        <img src={logoImg} alt="Logo" />
+        <div className="main-content">
+          <img src={logoImg} alt="Logo" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Button" />
+            Crie sua sala com Google
+          </button>
+          <div className="separator">
+            ou entre em uma sala
+          </div>
+          <form >
+            <input type="text"
+              placeholder="Digite o código da sala"
+            />
+            <button type="submit">Entrar na sala</button>
+          </form>
         </div>
-        <button>
-             <img src={googleIconImg} alt="Button" />
-          Crie sua sala com Google
-        </button>
-        <div>
-          ou entre em uma sala
-        </div>
-
-        <form >
-          <input type="text"
-          placeholder="Digite o código da sala"
-          />
-          <button type="submit">Entrar na sala</button>
-
-        </form>
-
       </main>
- </div>
-)
+    </div>
+  )
 }
 
 export default Home
